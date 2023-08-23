1061471a99a75d015516a13b7d8dc687187cfd74
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-08-23 01:12:39 +0100

    Create Tweet button component

---
+++ TweetButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TweetButton() {
+  const tweetURL =
+    "https://twitter.com/intent/tweet?text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+an+NPM+package.%0A%0AWhat%20Is%20a%20Code%20Block?%0A%0Ahttps%3A%2F%2Famzn.to/3qxnEdF";
+
+  return (
+    <div>
+      <a href={tweetURL} target="_blank">
+        Send a thank you tweet
+      </a>
+    </div>
+  );
+}
