4b2265923dca029934ebbff2c7e059ae6e111154
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-02-09 22:10:42 +0100

    chore: Change jest configuration file to typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+import type { Config } from "jest";
 
-module.exports = {
+const config: Config = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.css$": "jest-transform-css",
     "^.+\\.tsx?$": "ts-jest",
   },
 };
+
+export default config;
